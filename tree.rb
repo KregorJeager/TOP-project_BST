25147902561035b24d29e9a6504687e76a541b2d
kregor <kregorjeager@gmail.com>
2025-08-20 19:03:06 +0800

    add build_tree func which create a BST

--- tree.rb
+++ tree.rb
@@ -1,10 +1,37 @@
 # frozen_string_literal: true
 
+require_relative 'node'
 # Class for a BST
 class Tree
   attr_accessor :root
 
   def initialize(arr)
-    @root = build_tree(arr)
+    sorted_arr = arr.sort.uniq
+    @root = build_tree(sorted_arr)
+  end
+
+  def build_tree(arr)
+    create_bst(arr, 0, arr.length - 1)
+  end
+
+  def create_bst(arr, arr_start, arr_end)
+    return if arr_start > arr_end
+
+    arr_mid = (arr_start + arr_end) / 2
+    root = Node.new(arr[arr_mid])
+    root.left = create_bst(arr, arr_start, arr_mid - 1)
+    root.right = create_bst(arr, arr_mid + 1, arr_end)
+
+    root
+  end
+
+  def pretty_print(node = @root, prefix = '', is_left = true)
+    pretty_print(node.right, "#{prefix}#{is_left ? '│   ' : '    '}", false) if node.right
+    puts "#{prefix}#{is_left ? '└── ' : '┌── '}#{node.data}"
+    pretty_print(node.left, "#{prefix}#{is_left ? '    ' : '│   '}", true) if node.left
   end
 end
+
+test = Tree.new([3, 1, 2, 3, 4, 5, 6, 2, 3])
+
+test.pretty_print
